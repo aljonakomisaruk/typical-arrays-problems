4cd27224d3abc2132bf3cef8f2c2d713a1209acb
aljonakomisaruk <55145109+aljonakomisaruk@users.noreply.github.com>
2020-02-25 12:52:13 +0300

    feat: Typical Arrays Problems fix bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 
 exports.min = function min (array) {
-  return Math.min.apply(0, array);
+  if ((array instanceof Array) && !isFinite(array)){
+    return Math.min.apply(0, array);}
+    else  return 0;
+
 }
 
 exports.max = function max (array) {
-  return Math.max.apply(0, array);
+  if ((array instanceof Array) && !isFinite(array)){
+  return Math.max.apply(0, array);}
+  else  return 0;
 }
 
 exports.avg = function avg (array) {
   var summ=0;
+  if ((array instanceof Array) && !isFinite(array)){
   for (var i = 0; i < array.length; i++) {
       summ=summ+array[i];
   }
   return summ/i;
-
+  }
+  else  return 0;
 }
